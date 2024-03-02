fddfbf337e8962580d8f62f322367df9d88dba8e
Marcos García <markarules01@gmail.com>
2024-03-03 00:26:34 +0100

    Commit V16: inicializado el servicio de firebase en el proyecto para conectar con el servicio de autentificacion de usuario por medio de email y contraseña.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
-import 'package:flutter/cupertino.dart';
-import 'ActividadExamen.dart';
+import 'package:examen_final_psp_pmdm/ActividadExamen.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
-  runApp(ActividadExamen());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const ActividadExamen());
+}
+
+void initFB() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }

846d3a5fac01f8905dc3d8eb33aa843bc8d76667
Marcos García <markarules01@gmail.com>
2024-03-02 01:44:45 +0100

    Commit V4: creación de la vista login con los txtEmail y password.

---
+++ lib/OnBoarding/loginView.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class loginView extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('Inicio de Sesión'),
+        backgroundColor: Colors.blueGrey[700],
+        foregroundColor: Colors.white70,
+      ),
+      body: Center(
+        child: Container(
+          width: 300,
+          padding: EdgeInsets.all(60),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Proporciona tu email';
+                    }
+                    return null;
+                  },
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: Colors.white70),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white30),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Proporciona tu contraseña';
+                    }
+                    return null;
+                  },
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: 'Contraseña',
+                    labelStyle: TextStyle(color: Colors.white70),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white30),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 24.0),
+              ],
+            ),
+          ),
+        ),
+    );
+  }
+}

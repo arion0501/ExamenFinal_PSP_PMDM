5100c3cdf331682f89caa83ad0debf6a72d6cd6b
Marcos García <markarules01@gmail.com>
2024-03-05 00:26:42 +0100

    Commit V43: Modificado el CustomGridView para que muestre los producots de una manera visual mas atractiva.

--- lib/Custom/CustomGridView.dart
+++ lib/Custom/CustomGridView.dart
@@ -1,41 +1,69 @@
-import 'package:examen_final_psp_pmdm/FirestoreObjects/ProductosFS.dart';
 import 'package:flutter/material.dart';
+import '../FirestoreObjects/ProductosFS.dart';
 
 class CustomGridView extends StatelessWidget {
   final List<ProductosFS> productos;
   final int iPosicion;
-  final Function(int indice)? onItemListClickedFun;
+  final void Function(int index) onItemListClickedFun;
 
-  const CustomGridView(
-      {super.key, required this.productos,
-        required this.iPosicion,
-        required this.onItemListClickedFun,});
+  CustomGridView({
+    required this.productos,
+    required this.iPosicion,
+    required this.onItemListClickedFun,
+  });
 
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4,
-          mainAxisSpacing: 8,
-          crossAxisSpacing: 8,
-        ),
-        padding: const EdgeInsets.all(8),
-        itemCount: productos.length,
-        itemBuilder: (context, index) {
-          return InkWell(
-            child: Container(
-              color: Colors.blueGrey,
-              child: Center(
-                child: Text(
-                  productos[index].nombre,
-                  style: const TextStyle(fontSize: 20, color: Colors.white70),
-                ),
-              ),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 8,
+        crossAxisSpacing: 8,
+      ),
+      padding: const EdgeInsets.all(8),
+      itemCount: productos.length,
+      itemBuilder: (context, index) {
+        return InkWell(
+          child: Card(
+            color: Colors.blueGrey,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
             ),
-            onTap: () {
-              onItemListClickedFun!(index);
-            },
-          );
-        });
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(15.0),
+                  ),
+                  child: Image.network(
+                    productos[index].imagen,
+                    fit: BoxFit.cover,
+                    height: 110.08,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    productos[index].nombre,
+                    style: const TextStyle(fontSize: 16, color: Colors.white70),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Precio: ${productos[index].precio}\€',
+                    style: const TextStyle(fontSize: 14, color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          onTap: () {
+            onItemListClickedFun!(index);
+          },
+        );
+      },
+    );
   }
 }
